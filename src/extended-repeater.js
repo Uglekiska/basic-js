b8f8e285225bfe642e9e275990af9f5440e9606f
Uglekiska <uglekiska@yandex.by>
2021-10-01 17:56:11 +0300

    fix: complete extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
@@ -15,7 +17,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function repeater(str, options) {
+  // let str = 'str';
+  // let options = {
+  // 	repeatTimes: 3,
+  // 	separator: 'qq',
+  // 	addition: '-',
+  // 	additionRepeatTimes: 2,
+  // 	additionSeparator: 'A'
+  // }
+
+  //function match(str, options) {
+  let newStr = '';
+
+  if (!options.separator) {
+    options.separator = '+';
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    newStr = newStr + str;
+
+
+    if ((options.addition || options.addition === false || options.addition === null)) {
+      for (let index = 0; index < options.additionRepeatTimes; index++) {
+        newStr = newStr + options.addition;
+        if (options.additionRepeatTimes > 1 && index != options.additionRepeatTimes - 1) {
+          newStr = newStr + options.additionSeparator;
+        }
+      }
+    }
+
+    if (options.repeatTimes > 1 && i != options.repeatTimes - 1) {
+      newStr = newStr + options.separator;
+    }
+  }
+  //console.log(newStr);
+  return newStr;
+};
+//match(str, options);
+//}
