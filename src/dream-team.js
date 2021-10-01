9b271633810e668628423501fb13107adfbc1ff0
Uglekiska <uglekiska@yandex.by>
2021-10-01 19:24:45 +0300

    fix: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,9 +1,11 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
  *  
- * @param {Array} members names of the members 
+ * @param {membersay} members names of the members 
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
@@ -13,7 +15,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let letters = [];
+  let str = '';
+  let x = 0;
+
+  if (typeof members !== 'object' || members === null) {
+    return false;
+  };
+
+  for (let i = 0; i < members.length; i++) {
+    str = members[i];
+    if ((typeof str === "string" || str instanceof String) === false) {
+      x = x; //do nothing
+    } else {
+      let index = 0;
+      while (str[index] == ' ') {
+        index = index + 1;
+      }
+      letters.push(str[index].toUpperCase());
+    }
+  }
+  //let letters = names.map(s => s.slice(0, 1));
+  return letters.sort().join('');
 }
