9c37a6f24fb47a8dfe5d959712c02e2fade02fe6
Uglekiska <uglekiska@yandex.by>
2021-10-02 13:18:37 +0300

    fix: complete encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +12,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1] && str[i] !== str[i + 2] && str[i] !== str[i - 1]) {
+      arr.push(`2${str[i]}`);
+    } else if (str[i] === str[i + 1] && str[i] === str[i + 2] && str[i] !== str[i + 3] && str[i] !== str[i - 1]) {
+      arr.push(`3${str[i]}`);
+    } else if (str[i] === str[i + 1] && str[i] === str[i + 2] && str[i] === str[i + 3]) {
+      arr.push(`4${str[i]}`);
+    } else if (str[i] !== str[i + 1] && str[i] !== str[i - 1]) {
+      arr.push(`${str[i]}`);
+    }
+  }
+  return arr.join('');
 }
