67e2c6e4a21784a93040a4f57442274edae3d05f
Uglekiska <uglekiska@yandex.by>
2021-10-04 01:16:46 +0300

    fix: update some tasks files

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,9 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if(arr = []) {
+      return 1;
+    }
   }
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Create transformed array based on the control sequences that original
@@ -14,7 +16,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function transform(arr) {
-  if(Array.isArray(arr) === false) {
+  if (Array.isArray(arr) === false) {
     throw new Error('\'arr\' parameter must be an instance of the Array!');
   }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      newArr.push(arr[i]);
+    } else if (arr[i] === '--double-next' && i !== arr.length - 1) {
+      newArr.push(arr[i + 1])
+    } else if (arr[i] === '--discard-prev' && i !== 0 && arr[i - 2] !== '--discard-next') {
+      newArr.pop(arr[i - 1])
+    } else if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+      i = i + 1;
+    } else if (arr[i] === '--double-prev' && i !== 0) {
+      newArr.push(arr[i - 1])
+    }
+  }
+  return newArr;
 }
