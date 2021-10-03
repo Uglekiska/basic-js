1fb6ff369924d4a1b1c7c3b9d7d75394ffced440
Uglekiska <uglekiska@yandex.by>
2021-10-04 00:00:37 +0300

    fix: complete what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,7 +13,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) === '[object Date]') {
+    let mon = date.getMonth();
+    if (mon === 2 || mon === 3 || mon === 4) {
+      return 'spring';
+    }
+    if (mon === 5 || mon === 6 || mon === 7) {
+      return 'summer';
+    }
+    if (mon === 8 || mon === 9 || mon === 10) {
+      return 'autumn';
+    }
+    if (mon === 11 || mon === 1 || mon === 0) {
+      return 'winter';
+    }
+  } else if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('Invalid date!');
+  }
 }
